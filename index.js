3547fcfe26546ce8c5ab45dc23251aeda62c1c83
anamerasul <anamerasul@gmail.com>
2022-05-30 06:21:33 +0600

    all update

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const run = async () => {
     const carintrioReviewsCollection = client
       .db("car-intrio")
       .collection("reviews");
-    const carintrioUserssCollection = client
+    const carintrioUsersCollection = client
       .db("car-intrio")
       .collection("users");
 
@@ -109,7 +109,7 @@ const run = async () => {
 
     const verifyAdmin = async (req, res, next) => {
       const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({
+      const requesterAccount = await carintrioUsersCollection.findOne({
         email: requester,
       });
       if (requesterAccount.role === "admin") {
@@ -139,6 +139,79 @@ const run = async () => {
       res.send({ accessToken });
 
       console.log(accessToken);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await carintrioUsersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.NODE_ACCESS_JWT_TOKEN_SECRET,
+        { expiresIn: "4d" }
+      );
+      res.send({ result, token });
+    });
+
+    app.get("/user", async (req, res) => {
+      const users = await carintrioUsersCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.get("/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = await carintrioUsersCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await carintrioUsersCollection.updateOne(
+        filter,
+        updateDoc
+      );
+      res.send(result);
+    });
+
+    app.put(
+      "/user/removeadmin/:email",
+      verifyJWT,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: false },
+        };
+        const result = await carintrioUsersCollection.updateOne(
+          filter,
+          updateDoc
+        );
+        res.send(result);
+      }
+    );
+
+    // remove user
+
+    app.delete("/users/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carintrioUsersCollection.deleteOne(query);
+      res.send(result);
     });
 
     // add banner route
@@ -287,7 +360,7 @@ const run = async () => {
     //   res.send(product);
     // });
 
-    app.get("/products/:id", verifyJWT, async (req, res) => {
+    app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const item = await carintrioProductsCollection.findOne(query);
@@ -295,13 +368,325 @@ const run = async () => {
     });
 
     // update spefic product
-    app.put("/products/:id", verifyJWT, async (req, res) => {
+    app.put("/products/:id", async (req, res) => {
       const id = req.params.id;
       const data = req.body;
 
       const result = await carintrioProductsCollection.updateOne(
         { _id: new ObjectId(id) },
+
         { $set: data }
+      );
+
+      res.send(result);
+    });
+
+    // delete product
+
+    app.delete("/products/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carintrioProductsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // add add  user profile route
+
+    app.post("/adduserprofile", verifyJWT, async (req, res) => {
+      const adduserProfileInfo = req.body;
+
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+
+      console.log(Jwttokeninfo);
+      // console.log(orderInfo);
+      console.log(email);
+      const decoded = verifyJwtToken(
+        accessToken,
+        process.env.NODE_ACCESS_JWT_TOKEN_SECRET
+      );
+      console.log(decoded.email);
+
+      console.log(decoded.email);
+
+      if (decoded.email) {
+        const result = await carintriouserProfileCollection.insertOne(
+          adduserProfileInfo
+        );
+        res.send(result);
+      }
+      // if (email === decoded.email) {
+      //   const result = await orderCollection.insertOne(orderInfo);
+      //   res.send({ success: "Product ADD TO USER successfully" });
+      // } else {
+      //   res.status(403).send({ message: "forbidden access" });
+      // }
+    });
+
+    // get users profile route
+
+    app.get("/adduserProfile/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const id = req.params.id;
+      const query = {};
+      console.log(req);
+
+      const cursor = carintriouserProfileCollection.find({ email: email });
+
+      const profile = await cursor.toArray();
+
+      res.send(profile);
+    });
+
+    // edit user profile route
+    app.put("/adduserProfile/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const profile = req.body;
+
+      const result = await carintriouserProfileCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: profile }
+      );
+
+      res.send(result);
+    });
+
+    // add reviews route
+
+    app.post("/addreviews", verifyJWT, async (req, res) => {
+      const addreviews = req.body;
+
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+
+      console.log(Jwttokeninfo);
+      // console.log(orderInfo);
+      console.log(email);
+      const decoded = verifyJwtToken(
+        accessToken,
+        process.env.NODE_ACCESS_JWT_TOKEN_SECRET
+      );
+      console.log(decoded.email);
+
+      console.log(decoded.email);
+
+      if (decoded.email) {
+        const result = await carintrioReviewsCollection.insertOne(addreviews);
+        res.send(result);
+      }
+      // if (email === decoded.email) {
+      //   const result = await orderCollection.insertOne(orderInfo);
+      //   res.send({ success: "Product ADD TO USER successfully" });
+      // } else {
+      //   res.status(403).send({ message: "forbidden access" });
+      // }
+    });
+
+    // get all reviews
+
+    app.get("/addreviews", async (req, res) => {
+      const query = {};
+      const cursor = carintrioReviewsCollection.find(query);
+
+      const reviews = await cursor.toArray();
+
+      res.send(reviews);
+    });
+
+    // get spefic reviews route by id
+
+    app.get("/addreviews/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await carintrioReviewsCollection.findOne(query);
+      res.send(item);
+    });
+
+    //get spefic review by email
+    app.get("/addreviews/:email", verifyJWT, async (req, res) => {
+      // const email = req.params.email;
+      // const query = { email: email };
+      // const item = await carintrioReviewsCollection.findOne(query);
+      // res.send(item);
+
+      const email = req.params.email;
+      const id = req.params.id;
+      const query = {};
+      console.log(req);
+
+      const cursor = carintrioReviewsCollection.find({ email: email });
+
+      const profile = await cursor.toArray();
+
+      res.send(profile);
+
+      // const email = req.params.email;
+      // const id = req.params.id;
+      // const query = {};
+      // // console.log(req);
+
+      // const cursor = carintrioReviewsCollection.find({ email: email });
+
+      // const reviews = await cursor.toArray();
+
+      // res.send(reviews);
+    });
+
+    // delete review by id
+    app.delete("/addreviews/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carintrioReviewsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // edit review by id
+
+    app.put("/addreviews/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const review = req.body;
+
+      const result = await carintrioReviewsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: review }
+      );
+
+      res.send(result);
+    });
+
+    // add order route
+
+    app.post("/adduserorder", verifyJWT, async (req, res) => {
+      const order = req.body;
+
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+
+      console.log(Jwttokeninfo);
+      // console.log(orderInfo);
+      console.log(email);
+      const decoded = verifyJwtToken(
+        accessToken,
+        process.env.NODE_ACCESS_JWT_TOKEN_SECRET
+      );
+      console.log(decoded.email);
+
+      console.log(decoded.email);
+
+      if (decoded.email) {
+        const result = await carintrioOrdersCollection.insertOne(order);
+        res.send(result);
+      }
+    });
+
+    // get spefic order by route
+    app.get("/adduserorder/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const id = req.params.id;
+      const query = {};
+      console.log(req);
+
+      const cursor = carintrioOrdersCollection.find({ email: email });
+
+      const myorder = await cursor.toArray();
+
+      res.send(myorder);
+    });
+
+    // get all orders route
+
+    app.get("/adduserorder", async (req, res) => {
+      const query = {};
+      const cursor = carintrioOrdersCollection.find();
+
+      const order = await cursor.toArray();
+
+      res.send(order);
+    });
+
+    // get orders route
+
+    // app.get();
+
+    // update user order
+    app.put("/adduserorder/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const order = req.body;
+
+      const result = await carintrioOrdersCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: order }
+      );
+
+      res.send(result);
+    });
+
+    // delete order route by id
+
+    app.delete("/deleteorder/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const result = await carintrioOrdersCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+    // set payment info
+
+    app.post("/payment", verifyJWT, async (req, res) => {
+      const payment = req.body;
+
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+
+      console.log(Jwttokeninfo);
+      // console.log(orderInfo);
+      console.log(email);
+      const decoded = verifyJwtToken(
+        accessToken,
+        process.env.NODE_ACCESS_JWT_TOKEN_SECRET
+      );
+      console.log(decoded.email);
+
+      console.log(decoded.email);
+
+      if (decoded.email) {
+        const result = await carintrioPaymentsCollection.insertOne(payment);
+        res.send(result);
+      }
+    });
+
+    // get spefic order by route
+    app.get("/payment/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const id = req.params.id;
+      const query = {};
+      console.log(req);
+
+      const cursor = carintrioPaymentsCollection.find({ email: email });
+
+      const payment = await cursor.toArray();
+
+      res.send(payment);
+    });
+
+    //get all payments route
+    app.get("/payment", verifyJWT, async (req, res) => {
+      const query = {};
+      const cursor = carintrioPaymentsCollection.find(query);
+
+      const payment = await cursor.toArray();
+
+      res.send(payment);
+    });
+
+    // update payment info
+    app.put("/payment/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+
+      const result = await carintrioPaymentsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: payment }
       );
 
       res.send(result);
